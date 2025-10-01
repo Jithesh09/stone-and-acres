a267f917b07f8b681913a8013c44a02d3d46a8e6
nathangopi <nathangopi@users.noreply.github.com>
2025-10-01 11:50:41 +0530

    Added base url as VARAM in all the above files

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         {/* Home Page */}
-        <Route path="/" element={<Home />} />
+        <Route path="/varam" element={<Home />} />
 
         {/* Catch-all route: redirect wrong URLs to Home */}
         <Route path="*" element={<Navigate to="/" replace />} />
